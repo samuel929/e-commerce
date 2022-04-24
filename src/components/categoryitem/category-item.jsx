35b9744f4ae58c422bfcc3ae3c105c6d66d7e43e
Samuel <43027737+samuel929@users.noreply.github.com>
2022-04-24 15:30:13 +0200

    first commit

---
+++ src/components/categoryitem/category-item.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './category.styles.scss'
+function Categoryitem({category}) {
+    const{imageUrl,title} =category
+  return (
+    <div className='category-container'>
+    <div className='background-image' style={{backgroundImage:`url(${imageUrl})`}}/>
+    <div className='category-body-container'>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+    </div>
+ </div>
+  )
+}
+
+export default Categoryitem
